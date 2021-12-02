fd49bc615186da77b60e89e34e52c163ac3befc1
Marc Noirot <marc.noirot@gmail.com>
2021-12-02 15:11:06 +0100

    Add day 1 solutions

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -5,18 +5,35 @@ pub struct Problem;
 
 impl Solver for Problem {
     type Input = Vec<u32>;
-    type Output1 = u32;
-    type Output2 = u32;
+    type Output1 = usize;
+    type Output2 = usize;
 
     fn parse_input<R: Read>(&self, r: R) -> Self::Input {
         r.split_lines()
     }
 
     fn solve_first(&self, input: &Self::Input) -> Self::Output1 {
-        unimplemented!()
+        input
+            .windows(2)
+            .map(|c| match c {
+                [a, b] => a < b,
+                _ => false,
+            })
+            .filter(|&b| b)
+            .count()
     }
 
     fn solve_second(&self, input: &Self::Input) -> Self::Output2 {
-        unimplemented!()
+        input
+            .windows(3)
+            .map(|s| s.iter().sum())
+            .collect::<Vec<u32>>()
+            .windows(2)
+            .map(|c| match c {
+                [a, b] => a < b,
+                _ => false,
+            })
+            .filter(|&b| b)
+            .count()
     }
 }
